b09e0ccf410cd9215a6b4808a707de6cccc76a2f
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-09-16 14:56:24 -0400

    330 portfolio

---
+++ week1/js/story_editor.js
@@ -0,0 +1,19 @@
+function localStory() {
+  var storyName =document.getElementById("name_input").value
+  var storyHTML = localStorage.getItem(storyName)
+  document.getElementById("story_editor").value = storyHTML
+}
+function saveStory() {
+  var storyName = document.getElementById("name_input").value
+  var storyHTML = document.getElementById("story_editor").value
+  localStorage.setItem(storyName, storyHTML)
+}
+function displayStory() {
+  var storyHTML = document.getElementById("story_editor").value
+  document.getElementById("story_display").innerHTML = storyHTML
+}
+function loadStory() {
+  var storyName = document.getElementById("name_input").value
+  var storyHTML = localStorage.getItem(storyName)
+  document.getElementById("story_editor").value = storyHTML
+}
