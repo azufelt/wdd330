b9609794510fd55976001bec37d31290feea459c
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-09-23 20:40:37 -0400

    .

---
+++ week2/js/script.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+document.querySelector("#currentyear").innerHTML = date.getFullYear();
