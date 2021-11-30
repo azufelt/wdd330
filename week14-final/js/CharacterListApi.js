5e6b7e85c3e718073379a0c7a2bb048e8cfb36eb
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-11-29 23:40:51 -0500

    all content displays, included "if not", some imagese work, but had to splice, only working with jpeg OR png currently. Probably needs refactored, as I pull the same code 5 times over for each list. class attributes need to be added for styling purposes. Need to clear seearch bar upon finding the character, and clear the output results when new search is performed.

--- week14-final/js/CharacterListApi.js
+++ week14-final/js/CharacterListApi.js
@@ -13,13 +13,14 @@ function findChar() {
   //filter through the big List array to find an object that has a value that matches the string input from user;
   const filterList = bigCharacterList.filter(function (item) {
     if (item.name == searchChar) {
-      console.log(item.name)
       let listItem = document.querySelector('.returnresults');
 
       let li = document.createElement('li');
+      let title = document.createElement('h2');
       let liName = item.name;
-      li.append(liName);
-      listItem.append(li);
+      title.append(liName)
+      li.append(title)
+      listItem.append(li)
 
       let idNum = item.id;
       let getOneCharacter = `https://api.disneyapi.dev/characters/${idNum}`
@@ -27,48 +28,124 @@ function findChar() {
       fetch(getOneCharacter)
         .then((response) => response.json())
         .then(function (jsonObject) {
-          console.log(jsonObject);
-          if (jsonObject.films != "") {
-            let ul = document.createElement('ul');
+          //get img url and slice file extension
+          let CharImg = document.createElement('img');
+          let imgURL = jsonObject.imageUrl;
 
-            let liFilm = jsonObject.films;
-            for (i = 0; i < liFilm.length; i++) {
-              let filmList = document.createElement('li');
-              filmList.append(liFilm[i]);
-              ul.append(filmList)
+          let dummyString = imgURL;
+          dummyString = dummyString.slice(0, dummyString.lastIndexOf(".jpeg"))
+          let newURL = dummyString + ".jpeg";
+
+          CharImg.setAttribute('src', newURL);
+          CharImg.setAttribute('alt', 'disney character image')
+          li.append(CharImg)
+
+          //look for films and return them
+          if (jsonObject.films != "") {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = `Movies ${jsonObject.name} has been in:`;
+            let ul = document.createElement('ul');
+            ul.append(h2)
+            let objLi = jsonObject.shortFilms;
+            for (i = 0; i < objLi.length; i++) {
+              let objList = document.createElement('li');
+              objList.append(objLi[i]);
+              ul.append(objList)
+              li.append(ul)
+            }
+          } else {
+            return;
+          }
+          //Shortfilms
+          if (jsonObject.shortFilms != "") {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = `Short Films ${jsonObject.name} has been in:`;
+            let ul = document.createElement('ul');
+            ul.append(h2)
+            let objLi = jsonObject.shortFilms;
+            for (i = 0; i < objLi.length; i++) {
+              let objList = document.createElement('li');
+              objList.append(objLi[i]);
+              ul.append(objList)
+              li.append(ul)
+            }
+          } else {
+            return;
+          }
+          //tvShows
+          if (jsonObject.tvShows != "") {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = `TV shows ${jsonObject.name} has been in:`;
+            let ul = document.createElement('ul');
+            ul.append(h2)
+            let objLi = jsonObject.tvShows;
+            for (i = 0; i < objLi.length; i++) {
+              let objList = document.createElement('li');
+              objList.append(objLi[i]);
+              ul.append(objList)
               li.append(ul)
             }
           } else {
             return;
           }
 
+          //park attractions
+          if (jsonObject.parkAttractions != "") {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = `Park Attractions featuring ${jsonObject.name}:`;
+            let ul = document.createElement('ul');
+            ul.append(h2)
+            let objLi = jsonObject.parkAttractions;
+            for (i = 0; i < objLi.length; i++) {
+              let objList = document.createElement('li');
+              objList.append(objLi[i]);
+              ul.append(objList)
+              li.append(ul)
+            }
+          } else {
+            return;
+          }
+          //allies
+          if (jsonObject.allies != "") {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = `Park Attractions featuring ${jsonObject.name}:`;
+            let ul = document.createElement('ul');
+            ul.append(h2)
+            let objLi = jsonObject.allies;
+            for (i = 0; i < objLi.length; i++) {
+              let objList = document.createElement('li');
+              objList.append(objLi[i]);
+              ul.append(objList)
+              li.append(ul)
+            }
+          } else {
+            return;
+          }
+
+          //enemies
+          if (jsonObject.enemies != "") {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = `Park Attractions featuring ${jsonObject.name}:`;
+            let ul = document.createElement('ul');
+            ul.append(h2)
+            let objLi = jsonObject.enemies;
+            for (i = 0; i < objLi.length; i++) {
+              let objList = document.createElement('li');
+              objList.append(objLi[i]);
+              ul.append(objList)
+              li.append(ul)
+            }
+          } else {
+            return;
+          }
+
+
+
+
           searchChar.value = ""; //clear input value 
-
-          let CharImg = document.createElement('img');
-          let imgURL = jsonObject.imageUrl;
-          CharImg.setAttribute('src', imgURL);
-          CharImg.setAttribute('alt', 'disney character image')
-
-          li.append(CharImg)
-          console.log(imgURL);
         })
     }
-
-    // item.name == searchChar //True or False statement
-    // searchChar = "";
   });
-
-  // bigCharacterList.forEach(function (object) {
-  //   if (object.newItem.name == searchChar) {
-  //     // console.log('It works!')
-  //     let listItem = document.querySelector('.returnresults');
-
-  //     let li = document.createElement('li');
-  //     let liName = object.newItem.name;
-  //     li.append(liName);
-  //     listItem.append(li);
-  //   }
-  // })
 }
 
 
@@ -83,14 +160,10 @@ async function fetchList() {
   fetch(pageApi)
     .then((response) => response.json())
     .then(function (jsonObject) {
-      // console.log(jsonObject);
       totalPages = jsonObject.totalPages;
-      // console.log(totalPages);
-
       //build a new array for the characteer objects to go into
-      // console.log(bigCharacterList);
-
-      class item { //constructs the object to go into the big character list array
+      //constructs the object to go into the big character list array
+      class item {
         constructor(charName, charid) {
           this.name = charName;
           this.id = charid;
@@ -103,23 +176,20 @@ async function fetchList() {
           .then((respond) => respond.json())
           .then(function (jsObject) {
             const dataObj = jsObject['data'];
-            // console.log(dataObj); //returns list of JUST json object "data"
+            //returns list of JUST json object "data"
 
-            for (i = 0; i < dataObj.length; i++) { //loop through each object (on each page) and get the specific object value to populate the class constructor
+            //loop through each object (on each page) and get the specific object value to populate the class constructor
+            for (i = 0; i < dataObj.length; i++) {
               var charName = dataObj[i].name;
               var charid = dataObj[i]._id;
 
               let newItem = new item(charName, charid); //builds the object for the class constructor from withing the "for" statement and sends it to the global class constructor
               //... Instead of constructing a class, you could use this: let newItem = {name: charName, id: charid};
               bigCharacterList.push(newItem);
-              // console.log(bigCharacterList);
             }
-
           })
       }
-
     });
-  // console.log(bigCharacterList);
 }
 
 fetchList();
